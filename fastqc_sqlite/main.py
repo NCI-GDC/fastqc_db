1f79138baea55c0b3e0a6951e525cbe3b08cf8c6
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-04-18 17:35:13 -0500

    setup main

--- fastqc_sqlite/main.py
+++ fastqc_sqlite/main.py
@@ -9,7 +9,7 @@ from cdis_pipe_utils import pipe_util
 import fastqc_db
 
 def main():
-    parser = argparse.ArgumentParser('FastQC tool')
+    parser = argparse.ArgumentParser('FastQC to sqlite')
 
     # Logging flags.
     parser.add_argument('-d', '--debug',
@@ -25,24 +25,45 @@ def main():
                         required = True,
                         help = 'uuid string',
     )
-    parser.add_argument('-d', '--fastqc_data_path',
+    parser.add_argument('-d', '--fastqc_zip_path',
                         required=True
     )
-    parser.add_argument('-s', '--fastqc_summary_path',
-                        Required=True
+    
+
+    # setup required parameters
+    args = parser.parse_args()
+    uuid = args.uuid
+    fastqc_zip_path = args.fastqc_zip_path
+    parser.add_argument('--db_cred_s3url',
+                        required=False
+    )
+    parser.add_argument('--s3cfg_path',
+                        required=False
     )
 
     # setup required parameters
     args = parser.parse_args()
     uuid = args.uuid
-    fastqc_data_path = args.fastqc_data_path
-    fastqc_summary_path = args.fastqc_summary_path
+    fastqc_zip_path = args.fastqc_zip_path
+
+    if args.db_cred_s3url:
+        db_cred_s3url = args.db_cred_s3url
+    else:
+        db_cred_s3url = None
+    if args.s3cfg_path:
+        s3cfg_path = args.s3cfg_path
+
     
     logger = pipe_util.setup_logging(tool_name, args, uuid)
-    engine = pipe_util.setup_db(uuid)
 
-    fastqc_db.fastqc_db_data(uuid, fastqc_data_path, engine, logger)
-    fastqc_db.fastqc_db_summary(uuid, fastqc_summary_path, engine, logger)
+    if args.db_cred_s3url:
+        db_cred_s3url = args.db_cred_s3url
+    else:
+        db_cred_s3url = None
+    if args.s3cfg_path:
+        s3cfg_path = args.s3cfg_path
+
+    fastqc_db.fastqc_db(uuid, fastqc_zip_path, engine, logger)
     return
 
 
