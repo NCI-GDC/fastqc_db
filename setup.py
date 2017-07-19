8fe94a6ed827a83b68117b2b866534beb40f8329
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2017-07-19 14:46:40 -0500

    increment version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(name = 'fastqc_db',
       author = 'Jeremiah H. Savage',
       author_email = 'jeremiahsavage@gmail.com',
-      version = 0.29,
+      version = 0.31,
       description = 'store FastQC data/summary in sqlitedb',
       url = 'https://github.com/jeremiahsavage/fastqc_db',
       license = 'Apache 2.0',
