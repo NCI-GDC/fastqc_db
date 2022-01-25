e7f32ef0753dfc27f9b2b4ff0551b8cb8ae850d8
Charles Czysz <czysz@uchicago.edu>
2022-01-25 14:07:14 -0600

    Python files

--- setup.py
+++ setup.py
@@ -1,28 +1,10 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name = 'fastqc_db',
-      author = 'Jeremiah H. Savage',
-      author_email = 'jeremiahsavage@gmail.com',
-      version = 0.34,
-      description = 'store FastQC data/summary in sqlitedb',
-      url = 'https://github.com/jeremiahsavage/fastqc_db',
-      license = 'Apache 2.0',
-      packages = find_packages(),
-      install_requires = [
-          'pandas',
-          'sqlalchemy'
-      ],
-      classifiers = [
-          'Development Status :: 3 - Alpha',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: Apache Software License',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 3',
-      ],
-      entry_points={
-          'console_scripts': ['fastqc_db=fastqc_db.__main__:main']
-      }
+setup(
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
 )
+
+# __END__
