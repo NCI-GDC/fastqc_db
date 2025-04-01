588ea1e84b6248b54b4e30f35f57aceb6253ae9f
linghao <sxwcasd@gmail.com>
2025-04-01 20:33:14 +0000

    update test main

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,6 +8,6 @@ def test_get_total_deduplicated_percentage():
 
     logger = logging.getLogger("fastqc_db/tests")
 
-    with open("mock_data/test.txt", "r") as f:
+    with open("tests/mock_data/test.txt", "r") as f:
         splited_results = get_total_deduplicated_percentage(f, logger)
         assert splited_results[1] == "test successful"
